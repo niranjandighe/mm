7440a1d539e7356969db3bcc69031cf30477859c
Niranjan Dighe <niranjan.dighe@gmail.com>
2015-02-26 09:45:17 +0530

    Logical <--> Physical: Using PAGE_OFFSET, __pa() and __va

--- dma.c
+++ dma.c
@@ -4,16 +4,29 @@
 
 MODULE_LICENSE("GPL");
 
+static int *logical_address;
+static unsigned long physical_address;
+
 static __init int debugfs_init(void)
 {
+
+	logical_address = kmalloc(4096, GFP_KERNEL);
+
+	pr_warn("Logical address allocated = %p\n", logical_address);
+	pr_warn("PAGE_OFFSET = %p\n", PAGE_OFFSET);
+	pr_warn("Physical address could be = %p\n", 
+			(unsigned long)logical_address - PAGE_OFFSET);
+	pr_warn("Physical address using macros = %p\n", 
+			physical_address = __pa(logical_address));
+	pr_warn("Logical address using macros again = %p\n",
+			__va(physical_address));
 	return 0;
 }
 
 static __exit void debugfs_exit(void)
 {
+	kfree(logical_address);
 }
 
 module_init(debugfs_init);
 module_exit(debugfs_exit);
-
-
